4d0b0df58254a977e9ee9cd857a8818c87c7a62f
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-10-25 15:57:04 -0300

    alinhando tarefas por datas

--- MVC/controller.py
+++ MVC/controller.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from models import BancoDeDados
+from datetime import datetime, date, timedelta
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="view")
@@ -11,12 +13,21 @@ templates = Jinja2Templates(directory="view")
 def todo(request: Request):
     banco = BancoDeDados("dbsqlite")
     tarefas = banco.mostrar_tarefas()
+    data_de_hoje = datetime.now().strftime("%d/%m/%Y")
+    data_de_amanha = datetime.now() + timedelta(1)
+    data_de_amanha_formatada = data_de_amanha.strftime("%d/%m/%Y")
+        # for data in tarefas:
+        #     data_de_hoje = datetime.now().strftime("%d/%m/%Y")
+        #     if data[2] == data_de_hoje:
+        #         print("Hoje")
 
     return templates.TemplateResponse(
         "todo_list.html",
         {
             "request": request,
             "tarefas": tarefas,
+            "data_de_hoje": data_de_hoje,
+            "data_de_amanha_formatada": data_de_amanha_formatada,
         },
     )
 
